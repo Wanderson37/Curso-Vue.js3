23c056f1e55ffbc074385b1918e68d61a4935c58
Wanderson37 <wanderson_c.o@hotmail.com>
2024-12-28 18:57:32 -0400

    Aula5 add

---
+++ app/src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class='header'>
+        Header
+    </header>
+</template>
+<script>
+    export default {
+
+    }
+</script>
+<style>
+    .header {
+        background: black;
+        color: white;
+    }
+</style>
