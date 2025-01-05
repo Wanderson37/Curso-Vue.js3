f75f6e0867e56142428230bc99e76a22b2869437
Wanderson37 <wanderson_c.o@hotmail.com>
2025-01-05 08:04:59 -0400

    att site

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -39,17 +39,17 @@ export default createStore({
       })
     },
     addTodo({ commit }, data) {
-      return axios.post('http://localhost:3000/todos', data).then((response) => {
+      return axios.post('https://todo-list37.vercel.app/', data).then((response) => {
           commit('storeTodo', response.data)
       });
     },
     updateTodo({ commit }, { id, data }) {
-      return axios.put(`http://localhost:3000/todos/${id}`, data).then((response) => {
+      return axios.put(`https://todo-list37.vercel.app/${id}`, data).then((response) => {
         commit('storeTodo', response.data)
       })
     },
     deleteTodo({ commit },  id) {
-      return axios.delete(`http://localhost:3000/todos/${id}`).then(() => {
+      return axios.delete(`https://todo-list37.vercel.app/${id}`).then(() => {
         commit('deleteTodo', id)
       })
     }
